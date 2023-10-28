9e1ac2e5cf4c029cb3ce8e3819b4ca3b09236080
sky <chshmhsymabrda@gmail.com>
2023-10-28 23:20:15 +0330

    add binding

--- app/src/main/java/com/example/simpletextsaver/splash.kt
+++ app/src/main/java/com/example/simpletextsaver/splash.kt
@@ -2,10 +2,14 @@ package com.example.simpletextsaver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simpletextsaver.databinding.ActivityMainBinding
 
 class splash : AppCompatActivity() {
+    lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
